f14d09097585620bce59c44c41e3f38681b5f547
PAOLO <peolex4@hotmail.com>
2016-10-12 12:26:04 +0200

    second_commit

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
-
 // Few vars
-var vertMove = 83;
-var horMove = 101;
-var playerStartX = 200;
-var playerStartY = 380;
-var enemyStartX = -170;
+var VERT_MOVE = 83;
+var HOR_MOVE = 101;
+var HOR_STARTING_X = -170;
+var RESET_X = 200;
+var RESET_Y = 380;
 
-
-
-// Draw the enemy and player objects on the screen
-Object.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-//Reset player to initial position
-Object.prototype.reset = function() {
-  player.x = playerStartX;
-  player.y = playerStartY;
-}
-
-
-// Enemies class:
-var Enemy = function(x,y) {
-    this.sprite = 'images/enemy-bug.png';
+// Superclass
+var Character = function(sprite,x, y) {
+    this.sprite = sprite;
     this.x = x;
     this.y = y;
+};
+
+// Draw the enemy and player objects on the screen
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Player subclass:
+var Player = function(sprite, x, y){
+    Character.call(this, sprite, x, y);
+};
+
+// Player prototype to inherit Character.prototype's methods
+Player.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Player;
+
+
+// Enemies subclass:
+var Enemy = function(x,y) {
+    Character.call(this,'images/enemy-bug.png',x,y);
     this.speed = Math.floor(Math.random()*300+300);
 };
+
+// Enemy prototype to inherit Character.prototype's methods
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -37,7 +46,7 @@ Enemy.prototype.update = function(dt) {
     }
     //if not it keeps running
     else{
-        this.x = enemyStartX;
+        this.x = HOR_STARTING_X;
         this.speed = Math.floor(Math.random()*300+75);
         switch(Math.floor(Math.random()*3)){
             case 1:
@@ -50,52 +59,46 @@ Enemy.prototype.update = function(dt) {
                 this.y = 225;
             }
     }
-
     //If the player collides an enemy, reset the game
     if(player.x >= this.x - 30 && player.x <= this.x + 30){
         if(player.y >= this.y - 30 && player.y <= this.y + 30){
-            this.reset();
+            player.reset();
         }
     }
 };
-
-
-
-// Player class:
-var Player = function(){
-    this.sprite = 'images/char-horn-girl.png';
-    this.x = playerStartX;
-    this.y = playerStartY;
-}
-
 
 //Update player position
 Player.prototype.update = function(){
     //if left key is pressed and player is not on the first column, he moves 1 step on the left
     if(this.ctlKey === 'left' && this.x > 0){
-        this.x -= horMove;
+        this.x -= HOR_MOVE;
     //if right key is pressed and player is not on the last column, he moves 1 step on the right
     }else if(this.ctlKey === 'right' && !(this.x >= 400)){
-        this.x += horMove;
+        this.x += HOR_MOVE;
     //if up key is pressed player moves 1 step on the upper row
     }else if(this.ctlKey === 'up'){
-        this.y -= vertMove;
+        this.y -= VERT_MOVE;
     //if down key is pressed and player is not on the bottom row, he moves 1 step on row below
     }else if (this.ctlKey === 'down' && !(this.y >= 350)){
-        this.y += vertMove;
+        this.y += VERT_MOVE;
     }
     this.ctlKey = null;
-
     //If the player is on the water or he won, reset
     if(this.y < 25){
         this.reset();
     }
-}
+};
+
+//Reset player to initial position
+Player.prototype.reset = function() {
+  this.x = RESET_X;
+  this.y = RESET_Y;
+};
 
 //Input handler for player
 Player.prototype.handleInput = function(e){
     this.ctlKey = e;
-}
+};
 
 // Instantiate enemy objects
 var allEnemies = [];
@@ -118,8 +121,7 @@ var allEnemies = [];
 }());
 
 // Istantiate player object
-var player = new Player();
-
+var player = new Player('images/char-horn-girl.png',RESET_X,RESET_Y);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -133,7 +135,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-
-
